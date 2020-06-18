893140dc81dda4885f40a2c6eb1db9317cbf370e
Kai Boschung <kbo@expertsinside.com>
2020-06-18 10:54:12 +0200

    added snf data

--- Create-InputJson.ps1
+++ Create-InputJson.ps1
@@ -1,4 +1,17 @@
-$mapsData = Get-Content .\custom.geo.json | ConvertFrom-Json
-$found =$mapsData.features | ? {$_.properties.iso_a2 -eq "AL"}
-$found.properties | Add-Member -Name "SnfNumber" -Value 100 -MemberType NoteProperty
-$mapsData | ConvertTo-Json -Depth 32 | Set-Content ./modified.json
+$mapsData = Get-Content "./data/custom.geo.json" | ConvertFrom-Json
+$snfData = Import-Csv "./data/Internationality.csv" -Delimiter "|"
+
+foreach($row in $snfData){
+    if($row.Year -ne 2019) { continue }
+    $found = $null
+    $found = $mapsData.features | ? {$_.properties.iso_a2 -eq $row.CountryIsoCode}
+    
+    if($found -ne $null){
+        if($row.Type -eq "Collaboration") { 
+            $found.properties | Add-Member -Name "SnfCollaboration" -Value $row.Number -MemberType NoteProperty
+        }else{
+            $found.properties | Add-Member -Name "SnfFellowship" -Value $row.Number -MemberType NoteProperty
+        }
+    }
+}
+$mapsData | ConvertTo-Json -Depth 32 | Set-Content "./data/snf-internationality-2019.json" -Force
